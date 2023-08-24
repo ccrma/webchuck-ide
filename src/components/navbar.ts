d2cb435501c154edb7da9179eb6f86fca5f2d969
terry feng <tzfeng@ucsd.edu>
2023-08-24 11:45:32 -0700

    update navbar

--- src/components/navbar.ts
+++ src/components/navbar.ts
@@ -5,6 +5,8 @@ export function setupNavbar() {
     mobileMenu = document.querySelector<HTMLDivElement>("#mobileMenu");
     mobileMenuButton = document.querySelector<HTMLButtonElement>("#mobileMenuButton");
 
+    // Dropdowns
+
     // add event listeners
     mobileMenuButton?.addEventListener("click", () => {
         if (mobileMenu?.classList.contains("hidden")) {
@@ -14,3 +16,20 @@ export function setupNavbar() {
         }
     });
 }
+
+/*
+          <div class="relative">
+            <button href="#" class="px-2 py-2 block text-dark font-semibold rounded hover:text-sky-blue-200 transition sm:py-0 sm:mr-1">File</button>
+            <div class="absolute left-0 mt-2 py-1 w-48 bg-white rounded-lg shadow-sm">
+              <a href="#" class="block px-4 py-2 text-gray-700 hover:bg-gray-50 hover:text-sky-blue-200">New</a>
+              <a href="#" class="block px-4 py-2 text-gray-700 hover:bg-gray-50 hover:text-sky-blue-200">Upload</a>
+            </div>
+          </div>
+
+          attach state for open/close
+          be able to close dropdown by clicking outside
+*/
+const dropdownTemplate = document.createElement('dropdownTemplate');
+dropdownTemplate.innerHTML = `
+ 
+`
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { setupNavbar } from "./components/navbar";
 import { setupChuckBar } from "./components/chuckBar";
 
 class Main {
+
     constructor() {
         setupNavbar();
         setupChuckBar(document.querySelector<HTMLDivElement>("#chuck-bar")!);
         createEditor(document.querySelector<HTMLDivElement>("#editor")!);
-
     }
 
     init() {
