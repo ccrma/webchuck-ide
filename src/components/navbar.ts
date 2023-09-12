8fe9aae5ade08c8c2c65369806a1e19b225f825f
terry feng <tzfeng@ucsd.edu>
2023-09-11 19:04:09 -0700

    create Dark mode

--- src/components/navbar.ts
+++ src/components/navbar.ts
@@ -13,10 +13,35 @@ import Dropdown from "./dropdown";
  * @class NavBar
  */
 export default class NavBar {
+    public static darkModeToggle: HTMLButtonElement;
     constructor() {
         NavBar.buildDropdowns();
+
+        // Dark Mode
+        NavBar.darkModeToggle = document.querySelector<HTMLButtonElement>("#darkModeToggle")!;
+        NavBar.setColorScheme(); // read from local storage
+        NavBar.darkModeToggle.addEventListener("click", () => {
+            NavBar.toggleDarkMode();
+        });
     }
 
+    /**
+     * Set the color scheme of the page
+     */
+    static setColorScheme() {
+        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+        if ( localStorage.theme === "dark" || (!("theme" in localStorage ) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+        ) {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    }
+
+    /**
+     * Build all the dropdown components in the navbar
+     * Initializes the expand and collapse functionality
+     */
     static buildDropdowns() {
         // File
         let fileContainer =
@@ -62,5 +87,36 @@ export default class NavBar {
         let helpDropdown =
             document.querySelector<HTMLDivElement>("#helpDropdown")!;
         new Dropdown(helpContainer, helpButton, helpDropdown);
+    }
+
+    /**
+     * Disable WebChucK IDE dark mode
+     */
+    static darkModeOff() {
+        // turn off dark mode
+        localStorage.theme = "light";
+        NavBar.darkModeToggle.innerHTML = "Dark Mode: On";
+        this.setColorScheme();
+    }
+
+    /**
+     * Enable WebChucK IDE dark mode
+     */
+    static darkModeOn() {
+        // turn on dark mode
+        localStorage.theme = "dark";
+        NavBar.darkModeToggle.innerHTML = "Dark Mode: Off";
+        this.setColorScheme();
+    }
+
+    /**
+     * Switch between dark mode and light mode
+     */
+    static toggleDarkMode() {
+        if (localStorage.theme === "dark") {
+            this.darkModeOff();
+        } else {
+            this.darkModeOn();
+        }
     }
 }
