b767963cb90362ec91e8a22f0d6de2e184604c6e
terry feng <tzfeng@ucsd.edu>
2024-01-30 00:13:51 -0800

    fix delete button bg color

--- src/components/projectSystem.ts
+++ src/components/projectSystem.ts
@@ -182,6 +182,7 @@ export default class ProjectSystem {
             const fileOptions = document.createElement("div");
             const deleteButton = document.createElement("button");
             fileOptions.className = "fileExplorerOptions hide";
+            deleteButton.className = "fileEntryDelete";
             deleteButton.innerHTML = `<svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"> <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/> </svg>`;
             fileOptions.appendChild(deleteButton);
             fileOptions.addEventListener("click", (e) => {
