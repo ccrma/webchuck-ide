82cee32484df3332bea3ca943313586e0525ef16
terry feng <tzfeng@ucsd.edu>
2026-04-04 15:59:58 -0700

    Remove advanced options and url field input from shareModel

--- src/components/navbar/shareModal.ts
+++ src/components/navbar/shareModal.ts
@@ -8,15 +8,11 @@ export default class ShareModal {
     public static shareCodeButton: HTMLButtonElement;
     public static shareModal: HTMLDialogElement;
     public static shareCodeURLField: HTMLInputElement;
-    public static shareFileUrlField: HTMLInputElement;
     public static shareCodeCopyButton: HTMLButtonElement;
     public static shareProjectFilesCheckbox: HTMLInputElement;
     public static shareScopeCurrent: HTMLButtonElement;
     public static shareScopeAll: HTMLButtonElement;
     public static shareCurrentFilename: HTMLSpanElement;
-    public static advancedOptions: HTMLDetailsElement | null;
-    public static advancedBtn: HTMLButtonElement | null;
-    public static advancedBtnIcon: SVGSVGElement | null | undefined;
     public static closer: HTMLButtonElement;
 
     constructor() {
@@ -26,8 +22,6 @@ export default class ShareModal {
             document.querySelector<HTMLDialogElement>("#share-code-modal")!;
         ShareModal.shareCodeURLField =
             document.querySelector<HTMLInputElement>("#share-code-url")!;
-        ShareModal.shareFileUrlField =
-            document.querySelector<HTMLInputElement>("#share-file-url")!;
         ShareModal.shareCodeCopyButton =
             document.querySelector<HTMLButtonElement>("#share-code-copy")!;
         ShareModal.shareProjectFilesCheckbox =
@@ -38,14 +32,6 @@ export default class ShareModal {
             document.querySelector<HTMLButtonElement>("#share-scope-all")!;
         ShareModal.shareCurrentFilename =
             document.querySelector<HTMLSpanElement>("#share-current-filename")!;
-        ShareModal.advancedOptions = document.querySelector<HTMLDetailsElement>(
-            "#share-advanced-options"
-        );
-        ShareModal.advancedBtn = document.querySelector<HTMLButtonElement>(
-            "#share-advanced-btn"
-        );
-        ShareModal.advancedBtnIcon =
-            ShareModal.advancedBtn?.querySelector("svg");
         ShareModal.closer =
             document.querySelector<HTMLButtonElement>("#share-code-done")!;
 
@@ -56,20 +42,6 @@ export default class ShareModal {
      * Arm event listeners for the share modal
      */
     static initEventListeners() {
-        // Advanced Options
-        if (ShareModal.advancedBtn && ShareModal.advancedOptions) {
-            ShareModal.advancedBtn.addEventListener("click", () => {
-                ShareModal.advancedOptions!.open =
-                    !ShareModal.advancedOptions!.open;
-                if (ShareModal.advancedBtnIcon) {
-                    ShareModal.advancedBtnIcon.classList.toggle(
-                        "rotate-180",
-                        ShareModal.advancedOptions!.open
-                    );
-                }
-            });
-        }
-
         // Scope: Current
         if (ShareModal.shareScopeCurrent) {
             ShareModal.shareScopeCurrent.addEventListener("click", () => {
@@ -96,14 +68,8 @@ export default class ShareModal {
             });
         }
 
-        // Disable share project files if there are no .ck files
-
         // Save form input values before closing the dialog
         ShareModal.shareModal.addEventListener("close", () => {
-            sessionStorage.setItem(
-                "share-file-url",
-                ShareModal.shareFileUrlField.value
-            );
             sessionStorage.setItem(
                 "share-project-files",
                 ShareModal.shareProjectFilesCheckbox.checked.toString()
@@ -114,30 +80,9 @@ export default class ShareModal {
         ShareModal.shareCodeButton.addEventListener("click", () => {
             ShareModal.shareModal.showModal();
 
-            const urlParams = new URLSearchParams(window.location.search);
-            const urlParamUrl = urlParams.get("url");
-
-            if (sessionStorage.getItem("share-file-url") !== null) {
-                ShareModal.shareFileUrlField.value =
-                    sessionStorage.getItem("share-file-url") || "";
-            } else if (urlParamUrl !== null) {
-                ShareModal.shareFileUrlField.value = urlParamUrl;
-            }
-
             if (sessionStorage.getItem("share-project-files") !== null) {
                 ShareModal.shareProjectFilesCheckbox.checked =
                     sessionStorage.getItem("share-project-files") === "true";
-            }
-
-            if (ShareModal.advancedOptions) {
-                ShareModal.advancedOptions.open =
-                    ShareModal.shareFileUrlField.value !== "";
-                if (ShareModal.advancedBtnIcon) {
-                    ShareModal.advancedBtnIcon.classList.toggle(
-                        "rotate-180",
-                        ShareModal.advancedOptions.open
-                    );
-                }
             }
 
             const activeFile = ProjectSystem.getProjectFiles().find((f) =>
@@ -152,11 +97,6 @@ export default class ShareModal {
 
             ShareModal.updateShareURL();
             ShareModal.shareCodeURLField.setSelectionRange(0, 0);
-        });
-
-        // Update URL on input
-        ShareModal.shareFileUrlField.addEventListener("input", () => {
-            ShareModal.updateShareURL();
         });
 
         ShareModal.shareProjectFilesCheckbox.addEventListener("change", () => {
@@ -264,10 +204,6 @@ export default class ShareModal {
                 },
             ];
             urlParams.share = compressSharedFiles(shareFiles);
-        }
-
-        if (ShareModal.shareFileUrlField.value !== "") {
-            urlParams.url = ShareModal.shareFileUrlField.value;
         }
 
         ShareModal.shareCodeURLField.value = generateURLFromParams(urlParams);
